b8fe6d67d166969f62202fc84156acbd4b667c35
robotabc2 <bekbaganbetov.abay@gmail.com>
2017-05-17 12:17:48 +0600

    Habr RESP API 1 step

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -3,35 +3,115 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, jsonify
 from FlaskWebProject1 import app
 
 @app.route('/')
 @app.route('/home')
 def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
+	"""Renders the home page."""
+	return render_template(
+		'index.html',
+		title='Home Page',
+		year=datetime.now().year,
+	)
 
 @app.route('/contact')
 def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
+	"""Renders the contact page."""
+	return render_template(
+		'contact.html',
+		title='Contact',
+		year=datetime.now().year,
+		message='Your contact page.'
+	)
 
 @app.route('/about')
 def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+	"""Renders the about page."""
+	return render_template(
+		'about.html',
+		title='About',
+		year=datetime.now().year,
+		message='Your application description page.'
+	)
+
+@app.route('/api')
+def index():
+	return "Hello, World!"
+
+tasks = [
+	{
+		'id': 1,
+		'title': u'Buy groceries',
+		'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+		'done': False
+	},
+	{
+		'id': 2,
+		'title': u'Learn Python',
+		'description': u'Need to find a good Python tutorial on the web', 
+		'done': False
+	}
+]
+
+@app.route('/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+	return jsonify({'tasks': tasks})
+
+
+from flask import abort
+
+@app.route('/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+	task = list(filter(lambda t: t['id'] == task_id, tasks))
+	if len(task) == 0:
+		abort(404)
+	return jsonify({'task': task[0]})
+
+
+from flask import make_response
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+from flask import request
+@app.route('/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+@app.route('/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, tasks))
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+@app.route('/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
